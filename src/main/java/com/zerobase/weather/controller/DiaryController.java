b150c4d8bfa1ab3e3304d1e9411312111ccd4f12
younggwang Ji <gigi716@naver.com>
2024-03-26 23:47:32 +0900

    api

---
+++ src/main/java/com/zerobase/weather/controller/DiaryController.java
@@ -0,0 +1,30 @@
+package com.zerobase.weather.controller;
+
+import com.zerobase.weather.domain.Diary;
+import com.zerobase.weather.service.DiaryService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    public DiaryController(DiaryService diaryService) {
+        this.diaryService = diaryService;
+    }
+
+    @PostMapping("/create/diary")
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+                     @RequestBody String text) {
+        diaryService.createDiary(date, text);
+    }
+
+    @GetMapping("/read/diary")
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate date){
+       return diaryService.readDiary(date);
+    }
+}
